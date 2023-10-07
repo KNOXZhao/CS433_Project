1cebd8181f5ccad34e8fd37b71819d73f491970d
Knox <yzhaodx@connect.ust.hk>
2023-10-07 17:28:51 +0200

    Basic functions
    Add functions implemented in ex02 and ex03.
    Add a notebook test.ipynb for testing and debugging.

---
+++ project1/implementations.py
@@ -0,0 +1,348 @@
+# import necessary libraries
+import numpy as np
+
+# helper functions
+def load_data(sub_sample=True, add_outlier=False):
+    """Load data and convert it to the metric system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset,
+        delimiter=",",
+        skip_header=1,
+        usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1},
+    )
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5 / 0.454, 55.3 / 0.454]])
+
+    return height, weight, gender
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE
+    N= len(y)
+    e = y - tx.dot(w)
+    return 1/(2*N) * e.dot(e)
+    # ***************************************************
+
+def generate_w(num_intervals):
+    """Generate a grid of values for w0 and w1."""
+    w0 = np.linspace(-100, 200, num_intervals)
+    w1 = np.linspace(-150, 150, num_intervals)
+    return w0, w1
+
+def get_best_parameters(w0, w1, losses):
+    """Get the best w from the result of grid search."""
+    min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
+    return losses[min_row, min_col], w0[min_row], w1[min_col]
+
+def grid_search(y, tx, grid_w0, grid_w1):
+    """Algorithm for grid search.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        grid_w0: numpy array of shape=(num_grid_pts_w0, ). A 1D array containing num_grid_pts_w0 values of parameter w0 to be tested in the grid search.
+        grid_w1: numpy array of shape=(num_grid_pts_w1, ). A 1D array containing num_grid_pts_w1 values of parameter w1 to be tested in the grid search.
+
+    Returns:
+        losses: numpy array of shape=(num_grid_pts_w0, num_grid_pts_w1). A 2D array containing the loss value for each combination of w0 and w1
+    """
+
+    losses = np.zeros((len(grid_w0), len(grid_w1)))
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss for each combination of w0 and w1.
+    for i, w0 in enumerate(grid_w0):
+        for j, w1 in enumerate(grid_w1):
+            losses[i,j] = compute_loss(y, tx, np.array([w0, w1]))
+    return losses
+    # ***************************************************
+    
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute gradient vector
+    N = len(y)
+    e = y - tx.dot(w)
+    return -1/N * tx.T.dot(e)
+    # ***************************************************
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: compute gradient and loss
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        # ***************************************************
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: update w by gradient
+        w = w - gamma * gradient
+        # ***************************************************
+
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+    """
+
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: implement stochastic gradient computation. It's the same as the usual gradient.
+    return compute_gradient(y, tx, w)
+    # ***************************************************
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD).
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
+    """
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: implement stochastic gradient descent.
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size):
+            gradient = compute_stoch_gradient(y_batch, tx_batch, w)
+            loss = compute_loss(y_batch, tx_batch, w)
+            w = w - gamma * gradient
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+        # ***************************************************
+
+        print(
+            "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+    return losses, ws
+
+def compute_subgradient_mae(y, tx, w):
+    """Compute a subgradient of the MAE at w.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the subgradient of the MAE at w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute subgradient gradient vector for MAE
+    N = len(y)
+    e = y - tx.dot(w)
+    return -1/N * tx.T.dot(np.sign(e))
+    # ***************************************************
+    
+def subgradient_descent(y, tx, initial_w, max_iters, gamma):
+    """The SubGradient Descent (SubGD) algorithm.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SubGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SubGD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: compute subgradient and loss
+        gradient = compute_subgradient_mae(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        # ***************************************************
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: update w by subgradient
+        w = w - gamma * gradient
+        # ***************************************************
+
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "SubGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
+
+def stochastic_subgradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """The Stochastic SubGradient Descent algorithm (SubSGD).
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic subgradient
+        max_iters: a scalar denoting the total number of iterations of SubSGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SubSGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SubSGD
+    """
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: implement stochastic subgradient descent.
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size):
+            gradient = compute_subgradient_mae(y_batch, tx_batch, w)
+            loss = compute_loss(y_batch, tx_batch, w)
+            w = w - gamma * gradient
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+        # ***************************************************
+
+        print(
+            "SubSGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+    return losses, ws
+
